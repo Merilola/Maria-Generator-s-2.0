665d63079610609b5c636430b5080d8daad21886
Merilola <mariahef12@hotmail.com>
2023-02-09 17:17:13 +0000

    genrador

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let who1 = Math.floor(Math.random() * who.length);
+  let what1 = Math.floor(Math.random() * what.length);
+  let when1 = Math.floor(Math.random() * when.length);
+  document.querySelector("#excuse").innerHTML =
+    who[who1] + " " + what[what1] + " " + when[when1] + " ";
 };
