b7ade91e6e177bab8f4f88fbf74e861a0ae49abc
Merilola <mariahef12@hotmail.com>
2023-02-15 15:43:00 +0000

    funcionesflecha

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 window.onload = function() {
-  document.querySelector("#excuse").innerHTML = generateExcuse();
-};
-function generateExcuse() {
-  let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let what = ["eat", "pissed", "crushed", "broked"];
-  let when = [
+  const who = ["the dog", "my granma", "histurtle", "my bird"];
+  const what = ["eat", "pissed", "crushed", "broked"];
+  const when = [
     "before the class",
     "right in time",
     "when I finished",
     "during my lunch",
     "while I was praying"
   ];
-
-  let who1 = Math.floor(Math.random() * who.length);
-  let what1 = Math.floor(Math.random() * what.length);
-  let when1 = Math.floor(Math.random() * when.length);
-  return who[who1] + " " + what[what1] + " " + when[when1] + " ";
-}
+  const getNumberRandom = array => {
+    return Math.floor(Math.random() * array);
+  };
+  const getRandomItem = arr => {
+    return arr[getNumberRandom(arr.length)];
+  };
+  const getMessage = () => {
+    const sujetoRamdom = getRandomItem(who);
+    const verboRamdom = getRandomItem(what);
+    const accionRandom = getRandomItem(when);
+    const excusa = `${sujetoRamdom}
+   ${verboRamdom} ${accionRandom}`;
+    document.getElementById("excuse").innerHTML = excusa;
+  };
+  getMessage();
+};
